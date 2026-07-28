a9eeb28b6775f4cf41886f2619b01f4f0621daa0
hfutk <baotakeji@outlook.com>
2026-07-28 06:57:00 -0700

    Update ProtocolVersion and BuildVersion values

--- share/version.go
+++ share/version.go
@@ -9,12 +9,12 @@ import (
 //incompatible changes are made, this will
 //be incremented to signify a protocol
 //mismatch.
-var ProtocolVersion = "chisel-v3"
+var ProtocolVersion = "chiseell"
 
 //BuildVersion is set at build time via ldflags,
 //and otherwise falls back to the go module version
 //embedded by `go install pkg@version` (see init)
-var BuildVersion = "0.0.0-src"
+var BuildVersion = "1.1.1-src"
 
 func init() {
 	if info, ok := debug.ReadBuildInfo(); ok {
@@ -25,7 +25,7 @@ func init() {
 //fallbackVersion returns the go module version when the build
 //version was not stamped via ldflags
 func fallbackVersion(current string, info *debug.BuildInfo) string {
-	if current != "0.0.0-src" {
+	if current != "1.1.1-src" {
 		return current //stamped via ldflags
 	}
 	v := info.Main.Version
